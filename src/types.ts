cb0c44ff746c21d8160799bb26aae119cb2db1e3
nothingbyte <xiesweb@outlook.com>
2023-07-07 16:17:15 +0800

    update gpt4

--- src/types.ts
+++ src/types.ts
@@ -15,10 +15,9 @@ export interface ChatMessage {
 export type Role = "system" | "user" | "assistant" | "error"
 export type SimpleModel = "gpt-3.5" | "gpt-4"
 export type Model =
-  | "gpt-3.5-turbo-0613"
-  | "gpt-3.5-turbo-16k-0613"
-  | "gpt-4-0613"
-  | "gpt-4-32k-0613"
+  | "gpt-3.5-turbo"
+  | "gpt-3.5-turbo-16k"
+  | "gpt-4"
 
 export interface Prompt {
   desc: string
